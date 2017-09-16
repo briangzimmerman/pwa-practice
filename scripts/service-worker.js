77da9a6b34a5ea09e7793168a8fcc7c7e8a26580
Brian Zimmerman <briangzimmerman@gmail.com>
2017-09-15 23:58:36 -0500

    added list of files to cache

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,6 +1,23 @@
 let fileCacheName = 'pwa-practice-file-cache-1';
 let filesToCache = [
-    
+    '/',
+    '/fonts/material-icons/MaterialIcons-Regular.ttf',
+    '/fonts/material-icons/MaterialIcons-Regular.woff',
+    '/fonts/material-icons/MaterialIcons-Regular.woff2',
+    '/fonts/roboto/Roboto-Regular.woff',
+    '/fonts/roboto/Roboto-Regular.woff2',
+    '/images/me.jpg',
+    '/images/space.jpg',
+    '/scripts/app.js',
+    '/scripts/dexie.js',
+    '/scripts/event-handler.js',
+    '/scripts/jquery-3.2.1.min.js',
+    '/scripts/jquery.justifiedGallery.min.js',
+    '/scripts/materialize.min.js',
+    '/styles/inline.css',
+    '/styles/justifiedGallery.min.css',
+    '/styles/materialize.min.css',
+    '/index.html'
 ];
 
 self.addEventListener('install', (e) => {
